02ad0540475e34ca3a165dbbaa38f2301b0df1f3
Allan Robert Tan <argt@duck.com>
2024-03-11 06:24:19 +0000

    rename to app.exe

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Compile the project
-g++ -std=c++17 main.cpp DataStructs/DoublyLinkedList.hpp DataStructs/Node.hpp Song/Song.cpp Song/Song.hpp Playlist/Playlist.cpp Playlist/Playlist.hpp -o myprogram
+g++ -std=c++17 main.cpp DataStructs/DoublyLinkedList.hpp DataStructs/Node.hpp Song/Song.cpp Song/Song.hpp Playlist/Playlist.cpp Playlist/Playlist.hpp -o app.exe
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
     # If successful, run the program
     clear
-    ./myprogram
+    ./app.exe
 else
     # If compilation failed, print an error message
     echo "Compilation failed."
